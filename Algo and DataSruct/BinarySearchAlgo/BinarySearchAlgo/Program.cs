91d09075ccca44246b63ad9898b04482d025e6ef
Vadim <nikifovadim@gmail.com>
2022-04-05 08:47:16 +0300

    Upload new Algo

---
+++ Algo and DataSruct/BinarySearchAlgo/BinarySearchAlgo/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace BinarySearchAlgo
+{
+    /// Task desc
+    /// Given an array of integers nums which is sorted in ascending order, and an integer target,
+    /// write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+    /// You must write an algorithm with O(log n) runtime complexity.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example 1:
+            var firstNums = new int[]{ -1, 0, 3, 5, 9, 12 };
+            var frirstTarget = 9;
+            var caseOneExpected = 4;
+
+            var firstResult = BinarySearch.Search(firstNums, frirstTarget);
+
+            Console.WriteLine($"My own Algo Bin Search result is 4: {firstResult == caseOneExpected}");
+
+            var secondResult = Array.BinarySearch(firstNums, frirstTarget);
+
+            Console.WriteLine($"Default Algo Bin Search result is 4: {secondResult == caseOneExpected}");
+
+            // Example 1:
+            var secondNums = new int[] { 1, 3, 5, 6 };
+            var secondTarget = 5;
+            var caseTwoExpected = 2;
+
+            var thirdResult = BinarySearch.SearchInsert(secondNums, secondTarget);
+
+            Console.WriteLine($"My own Algo SearchInsert result is 2: {thirdResult == caseTwoExpected}");
+
+            // Example 2:
+            var thirdNums = new int[] { 1, 3, 5, 6 };
+            var thirdTarget = 2;
+            var caseThreeExpected = 1;
+
+            var fourthResult = BinarySearch.SearchInsert(thirdNums, thirdTarget);
+
+            Console.WriteLine($"My own Algo SearchInsert result is 1: {fourthResult == caseThreeExpected}");
+
+            Console.ReadKey();
+        }
+    }
+
+    public class BinarySearch
+    {
+        public static int Search(int[] nums, int target)
+        {
+            int left = default(int);
+            int right = nums.Length - 1;
+            int middle;
+
+            // O(log n)
+            while (left <= right)
+            {
+                // n / 2^k = 1 => k = log 2 n
+                middle = (right + left) / 2;
+
+                if (nums[middle] == target)      
+                    return middle;
+
+                if (nums[middle] > target)
+                    right = middle - 1;
+                else
+                    left = middle + 1;
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+        /// </summary>
+        /// <param name="nums">Your array</param>
+        /// <param name="target">Your numbet that you'd like to find</param>
+        /// <return>Best case: O(log n) Worst case: O(n)</return>>
+        public static int SearchInsert(int[] nums, int target)
+        {
+            // O(log n)
+            var result = Array.BinarySearch(nums, target);
+
+            if (result >= 0)
+               return result;
+
+            // O(n)
+            for (int index = 0; index < nums.Length; index++)
+            {
+                if (nums[index] > target)
+                    return index;
+            }
+
+            // O(1)
+            return nums.Length;
+        }
+
+        public static int RotatedSearch(int[] nums, int target)
+        {
+            return Array.FindIndex(nums, x => x == target);
+        }
+    }
+}
---
+++ Algo and DataSruct/VerifyingAlienDictionary/VerifyingAlienDictionary/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace VerifyingAlienDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstCaseWords = new string[] { "hello", "leetcode" };
+            var firstCaseOrder = "hlabcdefgijkmnopqrstuvwxyz";
+            var firstCaseExpected = true;
+
+            var firstCaseIsSorted = VerifyingDictionaryAlien.IsAlienSorted(firstCaseWords, firstCaseOrder);
+
+            var secondCaseWords = new string[] { "apple", "app" };
+            var secondCaseOrder = "abcdefghijklmnopqrstuvwxyz";
+            var secondCaseExpected = false;
+
+            var secondCaseIsSorted = VerifyingDictionaryAlien.IsAlienSorted(secondCaseWords, secondCaseOrder);
+
+            Console.WriteLine($"First case is sorted: {firstCaseExpected == firstCaseIsSorted}");
+            Console.WriteLine($"Second case is sorted: {secondCaseExpected != secondCaseIsSorted}");
+
+            Console.ReadKey();
+        }
+    }
+
+    public class AlienComparer : IComparer<string>
+    {
+        private string _order;
+        private Dictionary<char, int> _weightOfwords;
+
+        public AlienComparer(string order)
+        {
+            _order = order;
+
+            // O(1) because alpha is limited 
+            int counterWords = 0;
+            _weightOfwords = _order.ToDictionary(key => key, key => counterWords++);
+
+            // O(1) because alpha is limited 
+            //_weightOfwords = _order.ToDictionary(key => key, key => order
+            //    .ToList() // O(N)
+            //    .FindIndex(x => x == key)); // O(log N)
+        }
+
+        public int Compare([AllowNull] string x, [AllowNull] string y)
+        {
+            if (x == y)
+                return 0;
+
+            // O(M) where M is length of word
+            for (int i = 0; i < Math.Min(x.Length, y.Length); i++)
+            {
+                if (x[i] != y[i])
+                    return _weightOfwords[x[i]] < _weightOfwords[y[i]] ? -1 : 1;
+            }
+
+            return x.Length < y.Length ? -1 : 1;
+        }
+    }
+
+    public class VerifyingDictionaryAlien
+    {
+        // Return => O(N log N)
+        public static bool IsAlienSorted(string[] words, string order)
+        {
+            // A, B, C, D ...
+            // Be
+            // Deal
+            // Call
+            // Alphabet
+            // Asorting => ASC
+
+            // O(N log N)
+            var sortedWords = words.OrderBy(x => x, new AlienComparer(order));
+            // For SequenceEqual -> O(N)
+            return Enumerable.SequenceEqual(words, sortedWords);
+        }
+    }
+}
