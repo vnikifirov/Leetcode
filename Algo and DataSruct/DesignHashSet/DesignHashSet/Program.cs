039981f06a6fe68ac9f69d121a818f7baf35bee3
Vadim <nikifovadim@gmail.com>
2022-04-24 17:06:46 +0300

    Commit new Algo problems

---
+++ Algo and DataSruct/DesignHashSet/DesignHashSet/Program.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignHashSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myHashMap = new MyHashMap();
+            myHashMap.Put(1, 1); // The map is now [[1,1]]
+
+            var firstValue = myHashMap.Get(1);    // return 2, The map is now [[1,1], [2,2]]
+            Console.WriteLine($"Should be 1: {firstValue == 1}");
+
+            myHashMap.Put(2, 2); // The map is now [[1,1], [2,2]]
+
+            var secondValue = myHashMap.Get(2);    // return 2, The map is now [[1,1], [2,2]]
+            Console.WriteLine($"Should be 2: {secondValue == 2}");
+
+            myHashMap.Put(2, 1); // The map is now [[1,1], [2,1]] (i.e., update the existing value)
+
+            var ovewrited = myHashMap.Get(2);    // return 1, The map is now [[1,1], [2,1]]
+            Console.WriteLine($"Should be 1: {ovewrited == 1}");
+        }
+    }
+
+    /**
+     * Your MyHashSet object will be instantiated and called as such:
+     * MyHashSet obj = new MyHashSet();
+     * obj.Add(key);
+     * obj.Remove(key);
+     * bool param_3 = obj.Contains(key);
+     */
+    public class MyHashSet
+    {
+        /// <summary>
+        /// Size of Hash Table is 10^4
+        /// </summary>
+        private const int _size = 10_000;
+
+        private record Slot(int Key)
+        {
+            public Slot Next { get; set; } 
+        }
+
+        private Slot[] Slots;
+
+        public MyHashSet() => Slots = new Slot[_size];
+
+        public void Add(int key)
+        {
+            var hashKey = key.GetHashCode() % _size;
+            if (Slots[hashKey] is null)
+                Slots[hashKey] = new Slot(key);
+            else
+            {
+                var slot = Slots[hashKey];
+
+                // Singly LinkedList
+                while (slot.Next is not null)
+                {
+                    if (slot.Key == key)
+                        return;               
+
+                    slot = slot.Next;
+                }
+
+                if (slot.Key != key)
+                    slot.Next = new Slot(key);
+            }
+        }
+
+        public void Remove(int key)
+        {
+            var hashKey = key.GetHashCode() % _size;
+            if (Slots[hashKey] is not null)
+            {
+                var slot = Slots[hashKey];
+
+                if (slot.Key == key)
+                {
+                    Slots[hashKey] = slot.Next;
+                    return;
+                }
+
+                // Singly LinkedList
+                while (slot.Next is not null)
+                {
+                    if (slot.Next.Key == key)
+                    {
+                        // Node1(Curr) => Node2 = Node1(Curr) => Node2 => Node3
+                        slot.Next = slot.Next.Next;
+                        return;
+                    }
+
+                    slot = slot.Next;
+                }
+            }
+        }
+
+        public bool Contains(int key)
+        {
+            var hashKey = key.GetHashCode() % _size;
+            var slot = Slots[hashKey];
+
+            // Singly LinkedList
+            while (slot is not null)
+            {
+                if (slot.Key == key)
+                    return true;
+
+                slot = slot.Next;
+            }
+
+            return false;
+        }
+    }
+
+    public class MyHashMap
+    {
+        /// <summary>
+        /// Size of Hash Table is 10^4
+        /// </summary>
+        private const int _size = 10_000;
+
+        /// <summary>
+        /// Node of Hash Map
+        /// </summary>
+        private record Slot(int Key, int Value)
+        {
+            public int Value { get; set; } = Value;
+            /// <summary>
+            /// Next node of Hash Map
+            /// </summary>
+            public Slot Next { get; set; }
+        }
+
+        /// <summary>
+        /// Nodes of Hash Map
+        /// </summary>
+        private Slot[] Slots;
+
+        /// <summary>
+        /// CTOR of Hash Map
+        /// </summary>
+        public MyHashMap() => Slots = new Slot[_size];
+
+        public void Put(int key, int value)
+        {
+            var hashKey = key.GetHashCode() % _size;
+            if (Slots[hashKey] is null)
+                Slots[hashKey] = new Slot(key, value);
+            else
+            {
+                var slot = Slots[hashKey];
+
+                // Singly LinkedList
+                while (slot.Next is not null)
+                {
+                    if (slot.Key == key)
+                    {
+                        Slots[hashKey].Value = value;
+                        return;
+                    }
+
+                    slot = slot.Next;
+                }
+
+                if (slot.Key != key)
+                    slot.Next = new Slot(key, value);
+                else
+                    Slots[hashKey].Value = value;
+            }
+        }
+
+        public int Get(int key)
+        {
+            // hash div_remainder shift or sasear cipher (cryptography)
+            var hashKey = key.GetHashCode() % _size;
+            var slot = Slots[hashKey];
+
+            // Singly LinkedList
+            while (slot is not null)
+            {
+                if (slot.Key == key)
+                    return slot.Value;
+
+                slot = slot.Next;
+            }
+
+            return -1;
+        }
+
+        public void Remove(int key)
+        {
+            // Shift or Casear cipher (Cryptography)
+            var hashKey = key.GetHashCode() % _size;
+            if (Slots[hashKey] is not null)
+            {
+                var slot = Slots[hashKey];
+
+                if (slot.Key == key)
+                {
+                    Slots[hashKey] = slot.Next;
+                    return;
+                }
+
+                // Singly LinkedList
+                while (slot.Next is not null)
+                {
+                    if (slot.Next.Key == key)
+                    {
+                        // Node1(Curr) => Node2 = Node1(Curr) => Node2 => Node3
+                        slot.Next = slot.Next.Next;
+                        return;
+                    }
+
+                    slot = slot.Next;
+                }
+            }
+        }
+    }
+
+    /**
+     * Your MyHashMap object will be instantiated and called as such:
+     * MyHashMap obj = new MyHashMap();
+     * obj.Put(key,value);
+     * int param_2 = obj.Get(key);
+     * obj.Remove(key);
+     */
+}
---
+++ Algo and DataSruct/MinimumRoundsToCompleteAllTasks/MinimumRoundsToCompleteAllTasks/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MinimumRoundsToCompleteAllTasks
+{
+    class Program
+    {
+        /* 
+         * You are given a 0-indexed integer array tasks, where tasks[i] represents the difficulty level of a task.
+         * In each round, you can complete either 2 or 3 tasks of the same difficulty level.
+         * Return the minimum rounds required to complete all the tasks, or -1 if it is not possible to complete all the tasks.
+         * Example 1:
+            Input: tasks = [2,2,3,3,2,4,4,4,4,4]
+            Output: 4
+            Explanation: To complete all the tasks, a possible plan is:
+            - In the first round, you complete 3 tasks of difficulty level 2. 
+            - In the second round, you complete 2 tasks of difficulty level 3. 
+            - In the third round, you complete 3 tasks of difficulty level 4. 
+            - In the fourth round, you complete 2 tasks of difficulty level 4.  
+            It can be shown that all the tasks cannot be completed in fewer than 4 rounds, so the answer is 4.
+            Example 2:
+
+            Input: tasks = [2,3,3]
+            Output: -1
+            Explanation: There is only 1 task of difficulty level 2, but in each round, you can only complete either
+            2 or 3 tasks of the same difficulty level. Hence, you cannot complete all the tasks, and the answer is -1.
+         */
+        static void Main(string[] args)
+        {
+            var tasks = new int[] { 2, 3, 3 };
+            var result = MinimumRounds(tasks);
+
+            Console.WriteLine($"Rounds for exection of tasks: {result}");
+        }
+
+        public static int MinimumRounds(int[] tasks)
+        {
+            var sortedTasks = tasks
+                .OrderBy(task => task)
+                .ToList();
+            return MinimumRounds(sortedTasks);
+        }
+
+        private static int MinimumRounds(IList<int> tasks, int round = 0)
+        {
+            if (tasks.Count <= 0)
+                return round - 1;
+
+            var taskSameLvl = tasks.Where(task => tasks[0] == task);
+
+            if (taskSameLvl.Count() == 1)
+                return -1;
+            
+            else if (taskSameLvl.Count() <= 3)
+            {
+                for (int i = 0; i < taskSameLvl.Count(); i++)
+                    tasks.RemoveAt(i);
+            }
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                    tasks.RemoveAt(i);
+            }
+
+            var result = MinimumRounds(tasks, round + 1);
+
+            return result;
+        }
+    }
+}
